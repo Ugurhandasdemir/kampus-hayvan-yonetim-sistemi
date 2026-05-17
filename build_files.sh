b32af91c3644a1be40f9c3fbeda111305bcfd947
ugurhandasdemir <ugurhandasdemir@gmail.com>
2026-05-17 12:16:38 +0300

    Use --break-system-packages for pip in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,8 @@ set -e
 
 echo "BUILD START"
 
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
+python3.12 -m pip install --break-system-packages --upgrade pip
+python3.12 -m pip install --break-system-packages -r requirements.txt
 
 python3.12 manage.py collectstatic --noinput --clear
 
